7c17067aa30d63516a6807eb53f89e5a256b0e59
Orlitopro <124663445+Orlitopro@users.noreply.github.com>
2023-03-13 11:47:44 +0100

    Revert "Canvis recents"

--- Projecte/select_incidencia.php
+++ Projecte/select_incidencia.php
@@ -2,6 +2,6 @@
 
 $mysqli = include_once "connexio.php";
 
-$resultat = $mysqli->query("SELECT idInc, DEPARTAMENT.nom as aula, descripcio, DATE(dataIni) as dataIni FROM INCIDENCIA JOIN DEPARTAMENT ON aula = DEPARTAMENT.idDept WHERE dataFi IS NULL ORDER BY idInc ASC");
+$resultat = $mysqli->query("SELECT idInc, aula, descripcio, DATE(dataIni) as dataIni FROM INCIDENCIA WHERE dataFi IS NULL");
 $incidencies = $resultat->fetch_all(MYSQLI_ASSOC);
 return $incidencies;
--- select_incidencia.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$mysqli = include_once "connexio.php";
-
-$resultat = $mysqli->query("SELECT idInc, aula, descripcio, DATE(dataIni) as dataIni FROM INCIDENCIA WHERE dataFi IS NULL");
-$incidencies = $resultat->fetch_all(MYSQLI_ASSOC);
-return $incidencies;
