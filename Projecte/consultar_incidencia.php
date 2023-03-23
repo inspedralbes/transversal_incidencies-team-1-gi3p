1dd85914e51488b34e487dae73593bd3ea34a231
julsluks <124144517+julsluks@users.noreply.github.com>
2023-03-23 10:46:53 +0100

    Add files via upload

--- Projecte/consultar_incidencia.php
+++ Projecte/consultar_incidencia.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="insti icon" href="https://www.institutpedralbes.cat/wp-content/uploads/2021/05/logo.jpg">
     <?php include("includes.php")?>
-    <link rel="stylesheet" href="stylegrid.css">
     <title>GI3Pedralbes</title>
 </head>
     <body>
@@ -20,7 +19,7 @@
                 $resultado = $sentencia->get_result();
                 $incidencia = $resultado->fetch_assoc();
 
-                $resultatActuacio = $mysqli->query("SELECT descripcio, DATE(data) as data, temps FROM ACTUACIO WHERE incidencia = 1 AND visible = 1;
+                $resultatActuacio = $mysqli->query("SELECT descripcio, DATE(data) as data, temps FROM ACTUACIO WHERE incidencia = $id;
                 ");
                 $actuacions = $resultatActuacio->fetch_all(MYSQLI_ASSOC);
             }
@@ -32,12 +31,11 @@
             <h1 class="display-5 fw-bold">Consultar Incidència</h1>
             <div class="col-lg-6 mx-auto">
                 <p class="lead mb-4">Busca una incidència per ID</p>
-                <form action="" method="GET">
+                <form action="" method="GET" onSubmit="checkForm(event)" name="consultar">
                     <div class="form-floating mb-3">
                         <?php 
                             if(isset($_GET["id"])) { ?>
                                 <input type="text" class="form-control" id="floatingInput" placeholder="1" name="id" value="<?php echo $id ?>"> 
-                                
                                 <?php } else { ?>
                                     <input type="text" class="form-control" id="floatingInput" placeholder="1" name="id">
                                 <?php }
@@ -50,7 +48,7 @@
                     if(!$incidencia) { ?>
                         <p class="blockquote my-5">No existeix una incidència amb aquest ID!</p>
                     <?php } else { ?>
-                        <form action="tancar_incidencia_BBDD.php?id=<?php echo $id ?>" method="POST">
+                        <form action="tancar_incidencia_BBDD.php?id=<?php echo $id ?>" method="POST" id="resultat-incidencia">
                             <div class="card my-5">
                                 <div class="card-header bg-success-subtle">
                                     <h3>Descripció:</h3> <?php echo $incidencia["descripcio"]?>
@@ -66,38 +64,53 @@
                                     </div>
                                 </div>
                                 <div class="row">
-                                    <div class="col my-2 mx-5">
+                                    <div class="col my-3 mx-5">
                                         <input type="text" class="form-control text-center" value="Data inici: <?php echo $incidencia["dataIni"]?>" disabled >
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                        <?php 
-                        if(!$actuacions) { ?>
-                                <p class="blockquote my-5">No existeix cap actuacio en aquesta incidència</p>
+                            <?php
+                                if(!$actuacions) { ?>
+                                <p class="blockquote my-5">No existeix cap actuació amb aquest ID!</p>
                                 <?php } else { ?>
-                        <?php foreach ($actuacions as $actuacion) { ?>
-                            
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Temps</th>
-                                        <th scope="col">Descripció</th>
-                                        <th scope="col">Data</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><p><?php echo $actuacion["temps"]?> minuts</p></td>
-                                        <td><p><?php echo $actuacion["descripcio"]?></p></td>
-                                        <td><p><?php echo $actuacion["data"] ?></p></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        <?php include("footer.php")?>
-                <?php }}}}?>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Temps</th>
+                                            <th scope="col">Descripció</th>
+                                            <th scope="col">Data</th>
+                                        </tr>  
+                                    </thead>
+                                    <?php foreach ($actuacions as $actuacion) { ?> 
+                                    <tbody>                                  
+                                        <tr>                      
+                                            <td><p><?php echo $actuacion["temps"]?> minuts</p></td>
+                                            <td><p><?php echo $actuacion["descripcio"]?></p></td>
+                                            <td><p><?php echo $actuacion["data"] ?></p></td>
+                                        </tr>
+                                    </tbody>
+                                    <?php }}}}?>
+                                </table>
+                        </form>
+                         
+                
             </div>
         </div>
         <?php include("footer.php")?>
+
+        <script>
+            function checkForm(e) {
+                e.preventDefault()
+
+                let value = document.getElementById("floatingInput").value
+
+                if(!isNaN(value)) {
+                    document.forms["consultar"].submit()
+                } else {
+                    document.getElementById("floatingInput").value = 0
+                    document.forms["consultar"].submit()
+                }
+            }
+        </script>
     </body>
 </html>
