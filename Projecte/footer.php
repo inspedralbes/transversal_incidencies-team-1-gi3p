f2928f5ddb065713a80b71368c563dbdd4ff0eab
julsluks <124144517+julsluks@users.noreply.github.com>
2023-03-23 12:41:46 +0100

    Add files via upload

--- Projecte/footer.php
+++ Projecte/footer.php
@@ -3,7 +3,6 @@
       <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Inici</a></li>
       <li class="nav-item"><a href="insertar_incidencia.php" class="nav-link px-2 text-muted">Insertar incidència</a></li>
       <li class="nav-item"><a href="consultar_incidencia.php" class="nav-link px-2 text-muted">Consultar incidència</a></li>
-      <li class="nav-item"><a href="log_in.php" class="nav-link px-2 text-muted">Iniciar sessió</a></li>
     </ul>
 </footer>
 
