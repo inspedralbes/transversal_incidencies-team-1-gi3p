f81516a6871539fb7ece9239e6d2872b33073e57
julsluks <124144517+julsluks@users.noreply.github.com>
2023-03-23 10:33:55 +0100

    Add files via upload

--- Projecte/llistat_incidencia_grid.php
+++ Projecte/llistat_incidencia_grid.php
@@ -4,15 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include("includes.php")?>
     <link rel="stylesheet" href="stylesgrid.css">
     <link rel="insti icon" href="https://www.institutpedralbes.cat/wp-content/uploads/2021/05/logo.jpg">
     <title>Gestió incidències</title>
 </head>
 <body>
-<?php include("header.php")?>
+    <header>
+        <div class="header-title"><a href="/">Gestió Incidències</a></div>
+        <div class="header-list">
+            <div><a href="/">Inici</a></div>
+            <div><a href="perfil_professor.php">Professors</a></div>
+            <div><a href="perfil_administrador.php.php">Administradors</a></div>
+            <div><a href="llistat_tecnics.php">Tècnics</a></div>
+            <div><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">FAQs</a></div>
+        </div>
+    </header>
 
-    <?php $incidencies = include_once "select_incidencia.php"; ?>
+    <?php 
+        $mysqli = include_once "connexio.php";
+        $resultat = $mysqli->query("SELECT idInc, aula, descripcio, DATE(dataIni) as dataIni, prioritat FROM INCIDENCIA WHERE dataFi IS NULL ORDER BY prioritat DESC");
+        $incidencies = $resultat->fetch_all(MYSQLI_ASSOC);
+    ?>
 
     <div class="container">
         <div class="titulos">
@@ -25,7 +37,7 @@
             <?php foreach ($incidencies as $unaIncidencia) { ?>
                 <div class="editar">
                 <a href="modificar_incidencia.php?id=<?php echo $unaIncidencia["idInc"] ?>">
-                    <div class="incidencia">
+                    <div class="incidencia prioritat<?php echo $unaIncidencia["prioritat"] ?>">
                         <div class="id"><p><?php echo $unaIncidencia["idInc"] ?></p></div>
                         <div class="aula"><p><?php echo $unaIncidencia["aula"] ?></p></div>
                         <div class="descripcion"><p><?php echo $unaIncidencia["descripcio"] ?></p></div>
@@ -36,7 +48,14 @@
             <?php } ?>                            
         </div>
     </div>
-<?php include("footer.php")?>
+    
+    <footer>
+        <a href="index.php">Inici</a>
+        <a href="perfil_professor.php">Professors</a>
+        <a href="perfil_administrador.php">Administradors</a>
+        <a href="llistat_tecnics.php">Tecnics</a>
+        <a href="#">About</a>
+    </footer>
 </body>
 </html>
 
--- Projecte/log_in.php
+++ Projecte/log_in.php
@@ -16,8 +16,8 @@
         <h1 class="h3 mb-3 fw-normal">Inici de sessió</h1>
         <div class="col-lg-6 mx-auto">
             <div class="form-floating my-2">
-            <input type="email" class="form-control" placeholder="name@example.com">
-            <label for="floatingInput">Correu electrònic</label>
+            <input type="text" class="form-control" placeholder="name@example.com">
+            <label for="floatingInput">Usuari</label>
             </div>
             <div class="form-floating my-2">
             <input type="password" class="form-control" placeholder="Password">
