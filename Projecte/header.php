de7735c49df4db708b6ee3e86e26afbd41ca07df
julsluks <124144517+julsluks@users.noreply.github.com>
2023-03-27 13:43:16 +0200

    Add files via upload

--- Projecte/header.php
+++ Projecte/header.php
@@ -1,24 +1,41 @@
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom fixed-top bg-body-secondary text-emphasis-dark">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        <span class="fs-4">Gestió d'incidències</span>
-      </a>
+  <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+    <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+    <span class="fs-4">Gestió d'incidències</span>
+  </a> 
+      
 
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="log_in.php" class="nav-link active bg-warning text-dark" aria-current="page">Iniciar sessió</a></li>
-        <li class="nav-item"><a href="log_out.php" class="nav-link active bg-danger text-dark" aria-current="page">Tancar sessió</a></li>
-        <li class="nav-item"><a href="index.php" class="nav-link">Inici</a></li>
-        <li class="nav-item"><a href="insertar_incidencia.php" class="nav-link">Insertar incidència</a></li>
-        <li class="nav-item"><a href="consultar_incidencia.php" class="nav-link ">Consultar incidència</a></li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Més 
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="perfil_administrador.php">Administrador</a></li>
-            <li><a class="dropdown-item" href="llistat_tecnics.php">Tècnic</a></li>
-          </ul>
-        </li>
-      </ul>
+  <ul class="nav nav-pills">
+  <?php if(isset($_SESSION["permisos"])){
+                    
+    if($_SESSION["permisos"] == 1){ ?>
+            <li class="nav-item"><a href="log_out.php" class="nav-link active bg-danger text-white" aria-current="page">Tancar sessió</a></li>
+            <li class="nav-item"><a href="perfil_administrador.php" class="nav-link">Inici</a></li>
+            
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Opcions 
+              </a>
+              <ul class="dropdown-menu">
+                <li class="dropdown-item"><a href="llistat_incidencia_grid.php" class="nav-link">Llistat d'incidències</a></li>
+                <li class="dropdown-item"><a href="informe_tecnics.php" class="nav-link">Informe tècnics</a></li>
+                <li class="dropdown-item"><a href="consum_departaments.php" class="nav-link">Consum per departaments</a></li>
+              </ul>
+            </li>
 
+
+    <?php } else if($_SESSION["permisos"] == 2){ ?>
+          <li class="nav-item"><a href="log_out.php" class="nav-link active bg-danger text-white" aria-current="page">Tancar sessió</a></li>
+          <li class="nav-item"><a href="llistat_tecnics.php" class="nav-link">Insertar incidència</a></li>
+
+                
+    <?php }
+    } else { ?>
+            <li class="nav-item"><a href="log_in.php" class="nav-link active bg-warning text-white" aria-current="page">Iniciar sessió</a></li>
+            <li class="nav-item"><a href="index.php" class="nav-link">Inici</a></li>
+            <li class="nav-item"><a href="insertar_incidencia.php" class="nav-link">Insertar incidència</a></li>
+            <li class="nav-item"><a href="consultar_incidencia.php" class="nav-link ">Consultar incidència</a></li>
+  <?php }
+      ?>
+</ul>
 </header>
