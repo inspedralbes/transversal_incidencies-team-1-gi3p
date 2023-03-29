5c655f955b40c4709df498b166ac68b45aba831e
julsluks <124144517+julsluks@users.noreply.github.com>
2023-03-29 20:35:38 +0200

    Add files via upload

--- Projecte/index.php
+++ Projecte/index.php
@@ -1,5 +1,5 @@
 <?php session_start();
- if(isset($_SESSION["permisos"])){
+if(isset($_SESSION["permisos"])){
             
     if($_SESSION["permisos"] == 1){
         header("Location: perfil_administrador.php");
@@ -9,6 +9,11 @@
         
     }
 }
+
+$mysqli = include "connexio.php";
+
+$sequencia = $mysqli->query("SELECT idDept, nom, incidenciasObertes, incidenciasTotals FROM numIncidenciesPerDepartament");
+$resultat = $sequencia->fetch_all(MYSQLI_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -25,10 +30,21 @@
 <?php include("header.php")?>
     <div class="px-4 py-5 my-5 text-center">
         <h1 class="display-5 fw-bold py-5">G1 GI3Pedralbes</h1>
-       
+        <h2>Incidències de cada departament</h2>
+
+        <?php foreach($resultat as $unDepartament){ ?>
+            <div class="card" style="width: 18rem;">
+                <img src="./imatges/<?php echo $unDepartament['idDept'] ?>.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $unDepartament['nom'] ?></h5>
+                    <p class="card-text"><?php echo $unDepartament['incidenciasObertes'] ?>/<?php echo $unDepartament['incidenciasTotals'] ?></p>
+                </div>
+            </div>
+
+
+        <?php } ?>
         <a href="insertar_incidencia.php" class="btn btn-primary btn-lg px-4">Insertar incidència</a>
         <a href="consultar_incidencia.php" class="btn btn-primary btn-lg px-4">Consultar incidencia per ID</a>
-        <a href="log_out.php" class="btn btn-primary btn-lg px-4">Log out</a>
     </div>
 <?php include("footer.php")?>
 </body>
