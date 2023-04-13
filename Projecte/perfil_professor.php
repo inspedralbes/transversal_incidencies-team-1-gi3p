328caedbf853314ac392698d953e003493f07cf1
julsluks <124144517+julsluks@users.noreply.github.com>
2023-04-13 11:48:28 +0200

    Add files via upload

--- Projecte/perfil_professor.php
+++ Projecte/perfil_professor.php
@@ -1,4 +1,6 @@
-<?php session_start(); ?>
+<?php session_start(); 
+  $nomProfessor = $_SESSION["nom"];
+?>
 <!DOCTYPE html>
 <html lang="ca">
 <head>
@@ -13,8 +15,9 @@
 <?php include("header.php")?>
 
 <div class="px-4 py-5 my-5 text-center">
-  <h1 class="display-5 fw-bold my-4">Menú Professor</h1>
-    <div class="col-lg-6 mx-auto">
+  <h1 class="display-5 fw-bold my-4">Benvingut professor (<?php echo $nomProfessor ?>) !!</h1>
+    <div class="col-lg-6 mx-auto my-3">
+      <img src="https://imgs.search.brave.com/kdBYEtArcxLBS4q0NrMlLIS4HHWmonhMJHCrMEqy4UM/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5p/YVM4U1NJN181STJZ/ak00Skx0NGVBSGFI/YSZwaWQ9QXBp" class="rounded mx-auto d-block my-4" hight="150" width="150">
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
         <a href="insertar_incidencia.php" class="btn btn-primary btn-lg px-4 mx-3">Insertar incidencia</a>
         <a href="consultar_incidencia.php" class="btn btn-success btn-lg px-4 mx-3">Consultar incidencia</a>
