f08534ee6f27a393e5ec8c1b76ee531b25934b10
Orlitopro <124663445+Orlitopro@users.noreply.github.com>
2023-04-19 13:12:02 +0200

    Update log_in.php

--- Projecte/log_in.php
+++ Projecte/log_in.php
@@ -46,7 +46,7 @@ if(isset($_POST["username"])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ca">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
