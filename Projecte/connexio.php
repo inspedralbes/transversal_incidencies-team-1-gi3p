56eff7fb51a2e72ef4ec5616f835dad70dd24df3
julsluks <124144517+julsluks@users.noreply.github.com>
2023-04-19 12:54:23 +0200

    Update connexio.php

--- Projecte/connexio.php
+++ Projecte/connexio.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "db";
-$usuario = "root";
-$contrasenia = "1Password";
-$base_de_datos = "incidencies";
+$host = "localhost";
+$usuario = "a22arnfergil_gi3pgrup1";
+$contrasenia = "InsPedralbes2022";
+$base_de_datos = "a22arnfergil_gi3pgrup1";
 $mysqli = new mysqli($host, $usuario, $contrasenia, $base_de_datos);
 if ($mysqli->connect_errno) {
     echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
